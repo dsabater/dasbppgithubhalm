cfe08ee4c05a6a07dfc0fffa33e223597985ab73
daniel.sabater@gmail.com <daniel@dasabate.onmicrosoft.com>
2024-06-25 20:27:01 +0200

    chore: Refactor script to improve error handling in adding Entra users to environment

--- scripts/01_AddEntraUsersToEnvironment.ps1
+++ scripts/01_AddEntraUsersToEnvironment.ps1
@@ -22,7 +22,7 @@ foreach ($user in $users) {
         Write-Host "OK"
     }
     elseif ($result.Code -eq 400) {
-        Write-Error "FAIL" -NoNewline
+        Write-Error "FAIL"
         # Create a string with result.Error.message skipping the first 35 characters until the first appearance of string "The tracking Id is"
         $errorString = $result.Error.Message.Substring(32, $result.Error.Message.IndexOf("The tracking Id is") - 35)
         # Parse the errorstring variable into an object
